4e13ed1ea2386bcfccd3dcd846b2df3de19213df
tadghh <thenry.he@gmail.com>
2025-02-15 10:53:32 -0600

    nit

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    embed_resource::compile("resources.rc", embed_resource::NONE);
+    embed_resource::compile("icons/resources.rc", embed_resource::NONE);
 
     slint_build::compile("ui/main.slint").expect("Slint build failed")
 }
